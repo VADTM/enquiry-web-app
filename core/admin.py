d66b4583194ab68ed463db6d0c9169942ea4454b
ENADSOFT <adeboyeamos7@gmail.com>
2021-04-09 17:39:17 +0100

    Model student added

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import Student
 # Register your models here.
 
 #Admin Header
 admin.site.site_header = "Obafemi Awolowo University- Enquiry Admin Panel"
 
-
+#Register  Models
 admin.site.register(Student)
+
+#Unregister Models
+admin.site.unregister(Group)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    issue = models.CharField(max_length = 200)
+    Solution = models.TextField()
+    def __str__(self):
+        return self.name
+    def show_desc(self):
+        return self.description[:50]
