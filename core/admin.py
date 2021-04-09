8ea573e778cc5b859a9946a575ec7b8f74ee7074
ENADSOFT <adeboyeamos7@gmail.com>
2021-04-09 15:11:21 +0100

    next update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+
+#Admin Header
+admin.site.site_header = "Obafemi Awolowo University- Enquiry Admin Panel"
+
+
+admin.site.register(Student)
